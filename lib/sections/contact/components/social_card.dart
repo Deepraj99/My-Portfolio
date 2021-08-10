098618bb61fffae426f8324e2feedc8a3aa1c372
Deepak Prajapati <dp0774138@gmail.com>
2021-08-10 22:30:54 +0530

    fifth commit

---
+++ lib/sections/contact/components/social_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/constants.dart';
+
+class SocialCard extends StatefulWidget {
+  SocialCard({
+    Key? key,
+    required this.iconSrc,
+    required this.name,
+    required this.color,
+    required this.press,
+  }) : super(key: key);
+  final String iconSrc, name;
+  final Color color;
+  final Function press;
+
+  @override
+  _SocalCardState createState() => _SocalCardState();
+}
+
+class _SocalCardState extends State<SocialCard> {
+  bool isHover = false;
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+      child: InkWell(
+        // onTap: widget.press, //fix it 2
+        onHover: (value) {
+          isHover = value;
+        },
+        child: AnimatedContainer(
+          duration: Duration(microseconds: 200),
+          padding: EdgeInsets.symmetric(
+            vertical: kDefaultPadding / 2,
+            horizontal: kDefaultPadding * 1.5,
+          ),
+          decoration: BoxDecoration(
+            color: widget.color,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [if (isHover) kDefaultShadow],
+          ),
+          child: Row(
+            children: [
+              Image.asset(
+                widget.iconSrc,
+                height: 80,
+                width: 80,
+              ),
+              SizedBox(width: kDefaultPadding),
+              Text(widget.name),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
